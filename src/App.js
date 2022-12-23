d317bbb2514fb053dfa571fd66a64d4830a597e9
Patrick McCormick <pmmccormick7997@eagle.fgcu.edu>
2022-12-22 20:53:01 -0500

    implementing icons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,48 @@ function App() {
     const [data, setData] = useState({})
     const [location, setLocation] = useState('')
 
+    // pass JSON response as parameter and function will return the current flight rule
+    function flightRule(response) {
+        let flightStatus_paragraph = document.createElement('p')
+
+        if (response.flight_category === 'VFR') {
+            let flightStatus_text = document.createTextNode('VFR')
+            flightStatus_paragraph.appendChild(flightStatus_text)
+            document.getElementsByClassName('flight-rules')[0].appendChild(flightStatus_paragraph)
+            flightStatus_paragraph.style.color = '#1CC115'
+        }
+        else if (response.flight_category === 'MVFR') {
+            let flightStatus_text = document.createTextNode('MVFR')
+            flightStatus_paragraph.appendChild(flightStatus_text)
+            document.getElementsByClassName('flight-rules')[0].appendChild(flightStatus_paragraph)
+            flightStatus_paragraph.style.color = '#2CA4CA'
+        }
+        else if (response.flight_category === 'IFR') {
+            let flightStatus_text = document.createTextNode('IFR')
+            flightStatus_paragraph.appendChild(flightStatus_text)
+            document.getElementsByClassName('flight-rules')[0].appendChild(flightStatus_paragraph)
+            flightStatus_paragraph.style.color = '#E31111'
+        }
+        else if (response.flight_category === 'LIFR') {
+            let flightStatus_text = document.createTextNode('LIFR')
+            flightStatus_paragraph.appendChild(flightStatus_text)
+            document.getElementsByClassName('flight-rules')[0].appendChild(flightStatus_paragraph)
+            flightStatus_paragraph.style.color = '#753098'
+        }
+    }
+
+    function weatherIcon(weather) {
+        let image = document.createElement('img')
+
+
+        if (weather.clouds[0].text === 'Few' || weather.clouds[0].text === 'Scattered') {
+            console.log('few clouds')
+            image.src = require('./assets/few_clouds_day.svg').default
+            document.getElementsByClassName('temperature')[0].appendChild(image)
+        }
+    }
+
+    // takes user input and fetches JSON response for designated airport
     const searchAirport = (event) => {
 
         if (event.key === 'Enter') {
@@ -13,14 +55,15 @@ function App() {
                 .then((response) => response.json())
                 .then(response => {
                     if (response.data[0]) {
-                        console.log(response.data[0])
+                        // console.log(response.data[0])
                         // makes json data available for hook
                         setData(response.data[0])
-
-                        if (response.data[0] && response.data[0].flight_category === 'VFR') {
-
-                        }
+                        return response.data[0]
                     }
+                })
+                .then(response => {
+                    flightRule(response)
+                    weatherIcon(response)
                 })
         }
     }
@@ -42,7 +85,7 @@ function App() {
                     <p>{data.icao ? data.icao : null} - {data.station ? data.station.name : null}</p>
                     <p>{data.station ? data.station.location : null}</p>
                 </div>
-                <div className='temperature'>
+                <div className='weather'>
                     {data.temperature ? <h1>{data.temperature.fahrenheit}°F</h1> : null}
                 </div>
             </section>
@@ -54,28 +97,27 @@ function App() {
                 </div>
                 <div className="bottom-data">
                     <div className="flight-rules">
-                        {data.flight_category ? <p className='bold'>{data.flight_category}</p> : null}
                         <p>Flight Rules</p>
                     </div>
                     <div className="wind">
-                        {data.wind ? <p className='bold'>{data.wind.degrees}° at {data.wind.speed_kts} knots</p> : null}
                         <p>Wind</p>
+                        {data.wind ? <p className='bold'>{data.wind.degrees}° at {data.wind.speed_kts} knots</p> : null}
                     </div>
                     <div className="altimeter">
-                        {data.barometer ? <p className='bold'>{data.barometer.hg} inHg</p> : null}
                         <p>Altimeter</p>
+                        {data.barometer ? <p className='bold'>{data.barometer.hg} inHg</p> : null}
                     </div>
                     <div className="clouds">
-                        {data.clouds ? <p className='bold'>{data.clouds[0].text} {data.clouds[0].base_feet_agl}</p> : null}
                         <p>Clouds</p>
+                        {data.clouds ? <p className='bold'>{data.clouds[0].text} {data.clouds[0].base_feet_agl}</p> : null}
                     </div>
                     <div className="dewpoint">
-                        {data.dewpoint ? <p className='bold'>{data.dewpoint.fahrenheit}°F</p> : null}
                         <p>Dewpoint</p>
+                        {data.dewpoint ? <p className='bold'>{data.dewpoint.fahrenheit}°F</p> : null}
                     </div>
                     <div className="visibility">
-                        {data.visibility ? <p className='bold'>{data.visibility.miles}SM</p> : null}
                         <p>Visibility</p>
+                        {data.visibility ? <p className='bold'>{data.visibility.miles}SM</p> : null}
                     </div>
                 </div>
             </section>
