7456791ac9b62ecb9379f668657667a88c329fb1
pmac101 <pmac3187@gmail.com>
2022-12-24 08:38:45 -0500

    icon implementation complete

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import React, {useState} from 'react'
 function App() {
     //TODO: instead of creating elements in functions, do 'useState' and create elements in return html like originally done
     //TODO: eliminate duplicate text when submitting new query without refreshing page
-    const [data, setData] = useState({})
+    //TODO: add some effects when data loads
+    const [metarData, setMetarData] = useState({})
     const [location, setLocation] = useState('')
     const [rule, setRule] = useState('')
+    const [icon, setIcon] = useState('')
+    const [weather, setWeather] = useState('')
+    const [time, setTime] = useState('')
 
     // ----------------------------------------Functions----------------------------------------
 
@@ -29,64 +33,49 @@ function App() {
         }
     }
 
+    // checks checkwx API response for current cloud condition and also checks ipgeolocation API for current sunrise/sunset time. Returns appropriate image based on response data
     function weatherIcon(weather) {
-        let image = document.createElement('img')
-
-        if (weather.clouds[0] && (weather.clouds[0].text === 'Few' || weather.clouds[0].text === 'Scattered')) {
-            image.src = require('./assets/few_clouds_day.svg').default
-            document.getElementsByClassName('weather')[0].appendChild(image)
-        }
+        setIcon(weather.current.weather[0].icon)
     }
 
-    // retrieves local date/time
-    function dateTime(localInfo) {
-        let dateElement = document.createElement('p')
-        let dataElement_text = document.createTextNode(`Local time/date: ${localInfo.current_time.substring(0,5)} ${localInfo.date}`)
-
-        dateElement.appendChild(dataElement_text)
-        document.getElementsByClassName('airport')[0].appendChild(dateElement)
-        dateElement.style.fontSize = '1.2rem'
+    // converts unit timestamp to human-readable format
+    function timeConversion(unix) {
+        const milliseconds = unix.current.dt * 1000
+        const dateObject = new Date(milliseconds)
+        const readableTime = dateObject.toLocaleString()
+        setTime(readableTime)
     }
     // ----------------------------------------End Functions----------------------------------------
 
-    // takes user input and fetches JSON response for designated airport
+    // takes user input and makes 2 API calls to retrieve METAR and weather data
     const searchAirport = (event) => {
 
         if (event.key === 'Enter') {
+            // Call to CheckWX API - retrieves METAR data
             fetch(`https://api.checkwx.com/metar/${location}/decoded?x-api-key=429d0470f1b747b4b9de564ff9`)
                 .then((response) => response.json())
                 .then(response => {
                     if (response.data[0]) {
-                        // console.log(response.data[0])
-                        // makes json data available for hook
-                        setData(response.data[0])
+                        setMetarData(response.data[0])
                         return response.data[0]
                     }
                 })
                 .then(response => {
                     flightRule(response)
-                    weatherIcon(response)
                     return response
                 })
                 .then( response => {
-                    // ipgeolocation.io API: 30k requests per month, 1k daily. Used to retrieve local time
-                    fetch(`https://api.ipgeolocation.io/astronomy?apiKey=eedd7b3a85c34b0ca7438dfaa288a358&lat=${response.station.geometry.coordinates[1]}&long=${response.station.geometry.coordinates[0]}`)
-                        .then(response => response.json())
+                    // API call to openweathermap. Airport coordinates from CheckWX API response are used to retrieve current weather conditions
+                    fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${response.station.geometry.coordinates[1]}&lon=${response.station.geometry.coordinates[0]}&exclude=minutely&units=imperial&appid=f77078a3b256ade5ae31b9e8ab8422c6`)
+                        .then((response) => response.json())
                         .then(response => {
-                            dateTime(response)
-                            // console.log('date/time: ' + response.date_time_txt)
-                            // console.log('timezone: ' + response.timezone)
+                            setWeather(response.current.temp.toFixed(0))
+                            weatherIcon(response)
+                            timeConversion(response)
                         })
-
-                    // console.log(response.station.geometry.coordinates)
-                    // API call to openweathermap. Takes the coordinates from previous API and uses them to get other local data
-                    // fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${response.station.geometry.coordinates[1]}&lon=${response.station.geometry.coordinates[0]}&appid=f77078a3b256ade5ae31b9e8ab8422c6`)
-                    //     .then((response) => response.json())
-                    //     .then(response => {
-                    //
-                    //     })
                 })
         }
+
     }
 
     return (
@@ -103,11 +92,13 @@ function App() {
             {/*--------------------Middle section--------------------*/}
             <section className='top'>
                 <div className='airport'>
-                    <p>{data.icao ? data.icao : null} - {data.station ? data.station.name : null}</p>
-                    <p>{data.station ? data.station.location : null}</p>
+                    <h2>{metarData.icao ? metarData.icao +' -' : null} {metarData.station ? metarData.station.name : null}</h2>
+                    <h2>{metarData.station ? metarData.station.location : null}</h2>
+                    <h3 className='time'>{time}</h3>
                 </div>
                 <div className='weather'>
-                    {data.temperature ? <h1>{data.temperature.fahrenheit}°F</h1> : null}
+                    {icon !== '' ? <img className='weather-icon' src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="weather icon"></img> : null}
+                    {weather !== '' ? <h3>{weather}°F</h3> : null}
                 </div>
             </section>
             {/*--------------------Bottom section--------------------*/}
@@ -115,7 +106,7 @@ function App() {
             <section className='bottom'>
                 <div className="bottom-header">
                     <h3>METAR</h3>
-                    {data.raw_text ? <p className='bold'>{data.raw_text}</p> : null}
+                    {metarData.raw_text ? <p className='bold'>{metarData.raw_text}</p> : null}
                 </div>
                 <div className="bottom-data">
                     <div className="flight-rules">
@@ -125,23 +116,23 @@ function App() {
                     <div className="wind">
                         <p>Wind</p>
                         {/*TODO: need to fix this to display 'none' when there is no wind*/}
-                        {data.wind ? <p className='bold'>{data.wind.degrees}° at {data.wind.speed_kts} knots</p> : 'None'}
+                        {metarData.wind ? <p className='bold'>{metarData.wind.degrees}° at {metarData.wind.speed_kts} knots</p> : 'None'}
                     </div>
                     <div className="altimeter">
                         <p>Altimeter</p>
-                        {data.barometer ? <p className='bold'>{data.barometer.hg} inHg</p> : null}
+                        {metarData.barometer ? <p className='bold'>{metarData.barometer.hg} inHg</p> : null}
                     </div>
                     <div className="clouds">
                         <p>Clouds</p>
-                        {data.clouds ? <p className='bold'>{data.clouds[0].text} {data.clouds[0].base_feet_agl}</p> : null}
+                        {metarData.clouds ? <p className='bold'>{metarData.clouds[0].text} {metarData.clouds[0].base_feet_agl}</p> : null}
                     </div>
                     <div className="dewpoint">
                         <p>Dewpoint</p>
-                        {data.dewpoint ? <p className='bold'>{data.dewpoint.fahrenheit}°F</p> : null}
+                        {metarData.dewpoint ? <p className='bold'>{metarData.dewpoint.fahrenheit}°F</p> : null}
                     </div>
                     <div className="visibility">
                         <p>Visibility</p>
-                        {data.visibility ? <p className='bold'>{data.visibility.miles}SM</p> : null}
+                        {metarData.visibility ? <p className='bold'>{metarData.visibility.miles}SM</p> : null}
                     </div>
                 </div>
             </section>
