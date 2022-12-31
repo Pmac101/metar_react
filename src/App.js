43ece39078521dcfac798c7a3f7c4d6389a543c4
Patrick McCormick <pmmccormick7997@eagle.fgcu.edu>
2022-12-30 21:22:41 -0500

    functionality complete

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React, {useState} from 'react'
 
 //TODO: remove all commented console.logs
 function App() {
-    //TODO: instead of creating elements in functions, do 'useState' and create elements in return html like originally done
-    //TODO: eliminate duplicate text when submitting new query without refreshing page
-    //TODO: add some effects when data loads
     const [metarData, setMetarData] = useState({})
     const [location, setLocation] = useState('')
     const [rule, setRule] = useState('')
     const [icon, setIcon] = useState('')
-    const [weather, setWeather] = useState('')
+    const [temp, setTemp] = useState('')
     const [time, setTime] = useState('')
+    const [sky, setSky] = useState('')
 
     // ----------------------------------------Functions----------------------------------------
 
@@ -36,13 +34,17 @@ function App() {
     // checks checkwx API response for current cloud condition and also checks ipgeolocation API for current sunrise/sunset time. Returns appropriate image based on response data
     function weatherIcon(weather) {
         setIcon(weather.current.weather[0].icon)
+        setSky(weather.current.weather[0].description)
     }
 
     // converts unit timestamp to human-readable format
     function timeConversion(unix) {
-        const milliseconds = unix.current.dt * 1000
+        console.log('Unix time: ' + unix.current.dt)
+        const milliseconds = (unix.current.dt * 1000)
+        console.log('millisecond conversion: ' + milliseconds)
         const dateObject = new Date(milliseconds)
-        const readableTime = dateObject.toLocaleString()
+        const readableTime = dateObject.toLocaleString('en-US', {timeZone: `${unix.timezone}`})
+        console.log('dateObject readable time: ' + readableTime)
         setTime(readableTime)
     }
     // ----------------------------------------End Functions----------------------------------------
@@ -51,12 +53,16 @@ function App() {
     const searchAirport = (event) => {
 
         if (event.key === 'Enter') {
+            const welcome = document.getElementsByClassName('welcome-message')
+            welcome[0].style.visibility = 'hidden'
+
             // Call to CheckWX API - retrieves METAR data
             fetch(`https://api.checkwx.com/metar/${location}/decoded?x-api-key=429d0470f1b747b4b9de564ff9`)
                 .then((response) => response.json())
                 .then(response => {
                     if (response.data[0]) {
                         setMetarData(response.data[0])
+                        console.log(response.data[0])
                         return response.data[0]
                     }
                 })
@@ -69,13 +75,13 @@ function App() {
                     fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${response.station.geometry.coordinates[1]}&lon=${response.station.geometry.coordinates[0]}&exclude=minutely&units=imperial&appid=f77078a3b256ade5ae31b9e8ab8422c6`)
                         .then((response) => response.json())
                         .then(response => {
-                            setWeather(response.current.temp.toFixed(0))
+                            setTemp(response.current.temp.toFixed(0))
                             weatherIcon(response)
+                            console.log(response)
                             timeConversion(response)
                         })
                 })
         }
-
     }
 
     return (
@@ -90,7 +96,8 @@ function App() {
                     type='text'/>
             </section>
             {/*--------------------Middle section--------------------*/}
-            <section className='top'>
+            <section className='location-data'>
+                <h1 className='welcome-message'>Welcome! Enter an ICAO identifier in the box above to get started.</h1>
                 <div className='airport'>
                     <h2>{metarData.icao ? metarData.icao +' -' : null} {metarData.station ? metarData.station.name : null}</h2>
                     <h2>{metarData.station ? metarData.station.location : null}</h2>
@@ -98,25 +105,28 @@ function App() {
                 </div>
                 <div className='weather'>
                     {icon !== '' ? <img className='weather-icon' src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="weather icon"></img> : null}
-                    {weather !== '' ? <h3>{weather}°F</h3> : null}
+                    <div className="weather-conditions">
+                        {temp !== '' ? <h3>{temp}°F</h3> : null}
+                        {sky !== '' ? <p className='weather-description'>{sky}</p> : null}
+                    </div>
                 </div>
             </section>
             {/*--------------------Bottom section--------------------*/}
             {/*TODO: need to fix this so it does not show upon initial page load*/}
-            <section className='bottom'>
-                <div className="bottom-header">
+            <section className='metar'>
+                <div className="metar-header">
                     <h3>METAR</h3>
                     {metarData.raw_text ? <p className='bold'>{metarData.raw_text}</p> : null}
                 </div>
-                <div className="bottom-data">
+                <div className="metar-data">
                     <div className="flight-rules">
                         <p>Flight Rules</p>
                         {<p className='rule-color'>{rule}</p>}
                     </div>
                     <div className="wind">
                         <p>Wind</p>
-                        {/*TODO: need to fix this to display 'none' when there is no wind*/}
-                        {metarData.wind ? <p className='bold'>{metarData.wind.degrees}° at {metarData.wind.speed_kts} knots</p> : 'None'}
+                        {metarData.wind ?
+                            <p className='bold'>{metarData.wind.degrees}° at {metarData.wind.speed_kts} knots</p> : null}
                     </div>
                     <div className="altimeter">
                         <p>Altimeter</p>
@@ -124,7 +134,8 @@ function App() {
                     </div>
                     <div className="clouds">
                         <p>Clouds</p>
-                        {metarData.clouds ? <p className='bold'>{metarData.clouds[0].text} {metarData.clouds[0].base_feet_agl}</p> : null}
+                        {metarData.clouds ?
+                            <p className='bold'>{metarData.clouds[0].text} {metarData.clouds[0].base_feet_agl}</p> : null}
                     </div>
                     <div className="dewpoint">
                         <p>Dewpoint</p>
